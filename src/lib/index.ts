924e6524086e37542afb66d74c9b71367aa948a6
Joan Sentis <joansentis.dev@gmail.com>
2021-03-08 18:04:59 +0000

    create renderPathForWeigth function and IconWeigth type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,15 @@
 import { createContext, ComponentPropsWithoutRef } from "react";
 
+export { default as IconBase } from './IconBase';
+
+export type IconWeight = "thin" | "light" | "regular" | "bold" | "fill" | "duotone";
+
 export interface IconProps extends ComponentPropsWithoutRef<"svg"> {
   color?: string;
   size?: string | number;
-  weight?: "thin" | "light" | "regular" | "bold" | "fill" | "duotone";
+  weight?: IconWeight;
   mirrored?: boolean;
+  renderPath: Function
 }
 
 export type Icon = React.ForwardRefExoticComponent<
@@ -22,3 +27,21 @@ export const IconContext = createContext<IconContextProps>({
   weight: "regular",
   mirrored: false,
 });
+
+export const renderPathForWeigth = (
+  weight: IconWeight,
+  color: string,
+  pathsByWeigth: Map<IconWeight, Function>,
+): React.ReactNode | null => {
+  const path = pathsByWeigth.get(weight);
+
+  if (!!path) {
+    return path(color);
+  }
+
+  console.error(
+    'Unsupported icon weight. Choose from "thin", "light", "regular", "bold", "fill", or "duotone".'
+  );
+
+  return null;
+};
